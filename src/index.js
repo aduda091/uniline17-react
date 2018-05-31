55acf387194521f994078c90d83f7538751ffab0
Alen Duda <alenduda@gmail.com>
2018-06-01 00:37:00 +0200

    Remove service worker (gh-pages compatibility)

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import axios from "axios";
-import registerServiceWorker from "./registerServiceWorker";
+//import registerServiceWorker from "./registerServiceWorker";
 
 axios.defaults.baseURL = "https://uniline-17.herokuapp.com/api";
 ReactDOM.render(<App />, document.getElementById("root"));
-registerServiceWorker();
+//registerServiceWorker();
