824141c20e377692d3b7f21ce0157136ed2ab3da
Alen Duda <alenduda@gmail.com>
2018-05-31 21:57:56 +0200

    Change axios default url from localhost to Heroku

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import axios from 'axios';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import axios from "axios";
+import registerServiceWorker from "./registerServiceWorker";
 
-axios.defaults.baseURL = "http://localhost/uniline17/public/api";
-ReactDOM.render(<App />, document.getElementById('root'));
+axios.defaults.baseURL = "http://uniline-17.herokuapp.com/api";
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
